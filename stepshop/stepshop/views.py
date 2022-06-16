2e00a1b264c3898e8f50e58f692888f6be696f05
Oleg <collector1913@gmail.com>
2022-06-16 14:04:16 +0600

    add: hw_dj_09 files

--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -45,9 +45,15 @@ def index(request, pk=None):
 def contacts(request):
     title = 'контакты'
 
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     context = {
         'title': title,
         'links_menu': links_menu,
+        'basket': basket,
     }
 
     return render(request, 'contact.html', context)
@@ -56,9 +62,15 @@ def contacts(request):
 def about(request):
     title = 'о нас'
 
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     context = {
         'title': title,
         'links_menu': links_menu,
+        'basket': basket,
     }
 
     return render(request, 'about.html', context)
